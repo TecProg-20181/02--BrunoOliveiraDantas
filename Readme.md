cbb24c40464b4442fbd9aa78d0d82dd7753b406c
Bruna Moreira <brunanayaramlima@gmail.com>
2018-03-27 22:19:07 -0300

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Hangman
+
+Jogo: Forca.
+O jogador deve acertar a palavra proposta.
+É informada a quantidade de letrasna palavra em questão.
+O jogador deve tentar adivinhar letras que compoem a palavra.
+Caso acerte toda a palavra, ele vence. Caso contrário, perde.
+
+Projeto
+------
+
+O projeto contém dois arquivos principais.
+O código do programa está no arquivo `hang.py`.
+É necessário um arquivo que contenha as palavras que podem ser escolhidas.
+Nesse projeto estão dois arquivos de exemplo `words.txt` e `palavras.txt`.
+O arquivo a ser utilizado como pool de palavras está designado no código-fonte.
+
+Rodar o projeto
+------
+
+Para executar o projeto, use o comando:
+
+```
+python hang.py
+```
+
---
+++ hang.py
@@ -0,0 +1,117 @@
+import random
+import string
+
+WORDLIST_FILENAME = "palavras.txt"
+
+def loadWords():
+    """
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = string.split(line)
+    print "  ", len(wordlist), "words loaded."
+    return random.choice(wordlist)
+
+
+def isWordGuessed(secretWord, lettersGuessed):
+    secretLetters = []
+
+#    for letter in secretWord:
+#        if letter in secretLetters:
+#            secretLetters.append(letter)
+#        else:
+#            pass
+
+    for letter in secretWord:
+        if letter in lettersGuessed:
+            pass
+        else:
+            return False
+
+    return True
+
+def getGuessedWord():
+
+     guessed = ''
+
+
+     return guessed
+
+def getAvailableLetters():
+    import string
+    # 'abcdefghijklmnopqrstuvwxyz'
+    available = string.ascii_lowercase
+
+
+    return available
+
+def hangman(secretWord):
+
+    guesses = 8
+    lettersGuessed = []
+    print 'Welcome to the game, Hangam!'
+    print 'I am thinking of a word that is', len(secretWord), ' letters long.'
+    print '-------------'
+
+    while  isWordGuessed(secretWord, lettersGuessed) == False and guesses >0:
+        print 'You have ', guesses, 'guesses left.'
+
+        available = getAvailableLetters()
+        for letter in available:
+            if letter in lettersGuessed:
+                available = available.replace(letter, '')
+
+        print 'Available letters', available
+        letter = raw_input('Please guess a letter: ')
+        if letter in lettersGuessed:
+
+            guessed = getGuessedWord()
+            for letter in secretWord:
+                if letter in lettersGuessed:
+                    guessed += letter
+                else:
+                    guessed += '_ '
+
+            print 'Oops! You have already guessed that letter: ', guessed
+        elif letter in secretWord:
+            lettersGuessed.append(letter)
+
+            guessed = getGuessedWord()
+            for letter in secretWord:
+                if letter in lettersGuessed:
+                    guessed += letter
+                else:
+                    guessed += '_ '
+
+            print 'Good Guess: ', guessed
+        else:
+            guesses -=1
+            lettersGuessed.append(letter)
+
+            guessed = getGuessedWord()
+            for letter in secretWord:
+                if letter in lettersGuessed:
+                    guessed += letter
+                else:
+                    guessed += '_ '
+
+            print 'Oops! That letter is not in my word: ',  guessed
+        print '------------'
+
+    else:
+        if isWordGuessed(secretWord, lettersGuessed) == True:
+            print 'Congratulations, you won!'
+        else:
+            print 'Sorry, you ran out of guesses. The word was ', secretWord, '.'
+
+
+
+
+secretWord = loadWords().lower()
+hangman(secretWord)
